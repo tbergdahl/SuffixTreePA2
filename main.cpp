1d9f7d2aa20ff4344e4d9efcc57d7a8efc8f2c7f
Trenton Bergdahl <trenton.bergdahl@wsu.edu>
2025-04-20 20:34:06 -0700

    mat

--- main.cpp
+++ main.cpp
@@ -16,6 +16,14 @@ using namespace Alignment;
 
 namespace fs = std::filesystem;
 
+std::vector<std::string> parse_covid_files()
+{
+    std::vector<std::string> sequences;
+
+    // parse each fasta file
+}
+
+
 // used to find the start coordinates of common substring on s1
 int find_leaf_suffix_index(Node* node) {
     if (node->children.empty()) {
@@ -32,55 +40,16 @@ int find_leaf_suffix_index(Node* node) {
     return -1; // Shouldn't happen if the tree is valid
 }
 
-int main(int argc, char *argv[])
+
+
+int compute_similarity(std::string const& s1, std::string const& s2, std::string const& alphabet)
 {
-    char path_char;
-    #if defined(_WIN32)
-        path_char = '\\';
-    #else 
-        path_char = '/';
-    #endif
+    auto tree = Tree::build(s1, s2, alphabet);
 
-    // std::string cur_dir = fs::current_path().string();
-    // std::string test_dir = path_char + std::string("tests") + path_char + std::string("TestData") +path_char ;
-
-    // std::string fasta_filename    = cur_dir + test_dir + "Covid_Wuhan.fasta";
-    // std::string alphabet_filename = cur_dir + test_dir + "DNA_alphabet.txt";
-
-    // if(argc > 1)
-    // {
-    //     fasta_filename = cur_dir + test_dir + std::string(argv[1]);
-    // }
-    // if(argc > 2)
-    // {
-    //     alphabet_filename = cur_dir + test_dir + std::string(argv[2]);
-    // }
-
-    // Seq_alph_pair pair = parse(fasta_filename, alphabet_filename);
-    
-
-    // std::string output_filename = cur_dir + path_char + "output" + path_char + "outputs.txt";
-    // std::ofstream output_file(output_filename);
-
-
-    // if (!output_file.is_open()) {
-    //     std::cerr << "Error: Unable to open file for writing: " << output_filename << std::endl;
-    //     return 1;
-    // }
-
-    std::string s1 = "commonsubstring";
-    std::string s2 = "coammonxyz";
-    auto tree = Tree::build(s1, s2, "axyzcommonsubstring");
-
-    //std::cout << tree.enumerate_nodes();
-    //tree.display();
-
-
-    /*
+        /*
         Begin calculating a, b, c from assignment description
     */
     int a, b, c;
-
 
     Node* deepest = tree.find_deepest_shared_internal_node();
 
@@ -132,8 +101,61 @@ int main(int argc, char *argv[])
 
         c = Alignment::get_match_count(s_1_fwd, s_2_fwd, params);
     }
+    return (a + b + c);
+}
 
-    std::cout << "Similarity Score: " << a + b + c << std::endl;
+
+int main(int argc, char *argv[])
+{
+    char path_char;
+    #if defined(_WIN32)
+        path_char = '\\';
+    #else 
+        path_char = '/';
+    #endif
+
+    // std::string cur_dir = fs::current_path().string();
+    // std::string test_dir = path_char + std::string("tests") + path_char + std::string("TestData") +path_char ;
+
+    // std::string fasta_filename    = cur_dir + test_dir + "Covid_Wuhan.fasta";
+    // std::string alphabet_filename = cur_dir + test_dir + "DNA_alphabet.txt";
+
+    // if(argc > 1)
+    // {
+    //     fasta_filename = cur_dir + test_dir + std::string(argv[1]);
+    // }
+    // if(argc > 2)
+    // {
+    //     alphabet_filename = cur_dir + test_dir + std::string(argv[2]);
+    // }
+
+    // Seq_alph_pair pair = parse(fasta_filename, alphabet_filename);
+    
+
+    // std::string output_filename = cur_dir + path_char + "output" + path_char + "outputs.txt";
+    // std::ofstream output_file(output_filename);
+
+
+    // if (!output_file.is_open()) {
+    //     std::cerr << "Error: Unable to open file for writing: " << output_filename << std::endl;
+    //     return 1;
+    // }
+
+    std::string s1 = "commonsubstring";
+    std::string s2 = "coammonxyz";
+    
+
+    //std::cout << tree.enumerate_nodes();
+    //tree.display();
+
+
+   
+
+    using SimilarityMatrix = std::vector<std::vector<int>>;
+
+    
+
+    
 
     return 0;
 
